4badc481cc40ec5812c6475c47e2816ac2280fa4
BrandonLynchard <58010076+BrandonLynchard@users.noreply.github.com>
2021-12-01 21:04:03 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
+import mysql.connector
+import sys
+
+try:
+    connection = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="password",
+        database="final_project"
+    )
+
+    print("Successful connection.")
+
+
+except:
+    print("Failed connection.")
+
+    sys.exit()
+
+cursor = connection.cursor()
 
 
 class ShoppingCart:
@@ -15,8 +35,10 @@ class ShoppingCart:
         for item in self.items_list:
             # Loop through each item
             # Access the database to get its price using its name. The name is "item", the primary key
-            pass
-            price = 0
+            query = "SELECT price FROM books, movies WHERE name =%s"
+            data = (item,)
+            cursor.execute(query, data)
+            price = int(cursor)
             self.total += price
 
 
@@ -38,20 +60,29 @@ class ShoppingCart:
     def checkoutCart(self):
         self.items = ""
         self.items_list = []
-        # Push items = "" into the database for self.user
-        pass
+        
+        # Push items into the database
+        query = "UPDATE shopping_cart SET csv=%s  WHERE name=%s"
+        data = (self.items, self.user)
+        cursor.execute(query, data)
+        connection.commit()
 
     def addToCart(self, item):
         self.items_list.append(item)
         self.items = self.items + "," + item
 
         # Query the database for the price of the item, then add it to total
-        pass
-        price = 0
+        query = "SELECT price FROM books, movies WHERE name =%s"
+        data = (item,)
+        cursor.execute(query, data)
+        price = int(cursor)
         self.total += price
 
         # Push items into the database
-        pass
+        query = "UPDATE shopping_cart SET csv=%s  WHERE name=%s"
+        data = (self.items, self.user)
+        cursor.execute(query, data)
+        connection.commit()
 
 
     def removeFromCart(self, item):
@@ -62,13 +93,18 @@ class ShoppingCart:
             temp_str = temp_str + item + ","
         self.items = temp_str.rstrip(",")
 
-        # Query the database for the price of the item, then subtract it from total
-        pass
-        price = 0
+        # Retrieve the price for this item in the database
+        query = "SELECT price FROM books, movies WHERE name =%s"
+        data = (item,)
+        cursor.execute(query, data)
+        price = int(cursor)
         self.total -= price
 
         # Push items into the database
-        pass
+        query = "UPDATE shopping_cart SET csv=%s  WHERE name=%s"
+        data = (self.items, self.user)
+        cursor.execute(query, data)
+        connection.commit()
 
 
 
@@ -90,69 +126,6 @@ class ShoppingCart:
 
     def getNumItems(self):
         return self.numItems
-
-
-def Screen_One():
-    while True:
-        # Gather user input on what to do
-        print("Enter an integer")
-        print("(1) Login")
-        print("(2) Create an account")
-        print("(3) Exit program")
-        print()
-        user_input = str(input(""))
-
-        # Login: Moves to screen 2
-        if user_input == "1":
-            Screen_Two()
-
-        # Exit: Ends program
-        elif user_input == "2":
-            user_name = str(input("Enter username: "))
-            password = str(input("Enter password: "))
-            print("Moving back to title screen.")
-
-        # Create Account: Gathers information, then restarts screen 1
-        elif user_input == "3":
-            print("Goodbye!")
-            exit()
-
-        else:
-            print("Error: Enter a correct integer")
-
-def Screen_Two():
-    user_name = str(input("Enter username: "))
-    password = str(input("Enter password: "))
-
-    # If username/password passes
-    print("Success!, logging you in.")
-    Screen_Three()
-    return
-    # If username/password fails
-    # print("Failure: Returning to title screen.")
-    # return
-
-def Screen_Three():
-    print("Enter an integer")
-
-    # Requires a branch, no need to create new function
-    print("(1): View all items in a specific category")
-
-    print("(2): View all items in your shopping cart")
-
-    # Requires a branch, no need to create new function
-    print("(3): Add an item from a category to your shopping cart")
-
-    print("(4): Remove an item from your shopping cart")
-    print("(5): Checkout items currently in your shopping cart")
-    print("(6): View your order history")
-    print("(7): Edit your shipping information")
-    print("(8): Edit your payment information")
-    print("(9): Delete your account")
-    print("(10): Logout")
-
-# print("Hello!, Welcome to the store.")
-# Screen_One()
 
 
 
