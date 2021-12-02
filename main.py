473a902ae27a9ed149a48cf0e51855c8d7993630
Corbin-Rochelle <94238706+Corbin-Rochelle@users.noreply.github.com>
2021-12-01 18:04:42 -0600

    Update main.py

--- main.py
+++ main.py
@@ -3,68 +3,6 @@ import sys
 
 import order.py
 
-def main_menu():
-    print("""1) Search for Item\n
-    2) Search for Item Type\n
-    3) Add to Cart\n
-    4) View Cart\n
-    5) Return an Item\n
-    6) Checkout\n
-    7) Logout
-    """)
-    option = int(input())
-    match option:
-        case 1:
-            search_item()
-        case 2:
-            search_item_type()
-        case 3:
-            add_item_cart()
-        case 4:
-            view_cart()
-        case 5:
-            return_item()
-        case 6:
-            checkout()
-        case 7:
-            logout()
-
-
-def search_item():
-    # INSERT SQL QUERY HERE
-    print("NOT DONE")
-
-
-def search_item_type():
-    # INSERT SQL QUERY HERE
-    print("NOT DONE")
-
-
-def add_item_cart():
-    # INSERT SQL QUERY HERE
-    print("NOT DONE")
-
-
-def view_cart():
-    # INSERT SQL QUERY HERE
-    print("NOT DONE")
-
-
-def return_item():
-    # INSERT SQL QUERY HERE
-    print("NOT DONE")
-
-
-def checkout():
-    # INSERT SQL QUERY HERE
-    print("NOT DONE")
-
-
-def logout():
-    # INSERT SQL QUERY HERE
-    print("NOT DONE")
-
-
 # Connecting to the Database main
 try:
     connection = mysql.connector.connect(
@@ -86,4 +24,65 @@ cursor = connection.cursor()
 
 # Calling the menuing
 
-main_menu()
+def Screen_One():
+    while True:
+        # Gather user input on what to do
+        print("Enter an integer")
+        print("(1) Login")
+        print("(2) Create an account")
+        print("(3) Exit program")
+        print()
+        user_input = str(input(""))
+
+        # Login: Moves to screen 2
+        if user_input == "1":
+            Screen_Two()
+
+        # Exit: Ends program
+        elif user_input == "2":
+            user_name = str(input("Enter username: "))
+            password = str(input("Enter password: "))
+            print("Moving back to title screen.")
+
+        # Create Account: Gathers information, then restarts screen 1
+        elif user_input == "3":
+            print("Goodbye!")
+            exit()
+
+        else:
+            print("Error: Enter a correct integer")
+
+def Screen_Two():
+    user_name = str(input("Enter username: "))
+    password = str(input("Enter password: "))
+
+    # If username/password passes
+    print("Success!, logging you in.")
+    Screen_Three()
+    return
+    # If username/password fails
+    # print("Failure: Returning to title screen.")
+    # return
+
+
+def Screen_Three():
+    print("Enter an integer")
+
+    # Requires a branch, no need to create new function
+    print("(1): View all items in a specific category")
+
+    print("(2): View all items in your shopping cart")
+
+    # Requires a branch, no need to create new function
+    print("(3): Add an item from a category to your shopping cart")
+
+    print("(4): Remove an item from your shopping cart")
+    print("(5): Checkout items currently in your shopping cart")
+    print("(6): View your order history")
+    print("(7): Edit your shipping information")
+    print("(8): Edit your payment information")
+    print("(9): Delete your account")
+    print("(10): Logout")
+
+print("Hello!, Welcome to the store.")
+Screen_One()
