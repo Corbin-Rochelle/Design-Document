f38426880a8c8f29b43421a62d989aee720ed168
Connor Moody <89549880+mcmoody@users.noreply.github.com>
2021-12-01 20:06:36 -0600

    Update user.py

--- user.py
+++ user.py
@@ -5,10 +5,6 @@ class User:
 
 	def __init__(self):
 		self.firstName = ''
-		self.lastName = ''
-		self.email = ''
-		self.password = ''
-		self.address = ''
 
 	def getFirstName(self):
         query = "SELECT firstName FROM user WHERE name=%s"
@@ -16,56 +12,8 @@ class User:
         cursor.execute(query,data)
         return str(cursor)
 
-	def getLastName(self):
-        query = "SELECT lastName FROM user WHERE name=%s"
-        data = (self.lastName, )
-        cursor.execute(query,data)
-        return str(cursor)
-
-	def getEmail(self):
-        query = "SELECT email FROM user WHERE name=%s"
-        data = (self.email, )
-        cursor.execute(query,data)
-        return str(cursor)
-
-	def getPassword(self):
-        query = "SELECT password FROM user WHERE name=%s"
-        data = (self.password, )
-        cursor.execute(query,data)
-        return str(cursor)
-
-	def getAddress(self):
-        query = "SELECT address FROM user WHERE name=%s"
-        data = (self.address, )
-        cursor.execute(query,data)
-        return str(cursor)
-
 	def setFirstName(self,fname):
 		query = "UPDATE firstName SET user=%s  WHERE name=%s"
-        data = (user, self.name,)
-        cursor.execute(query, data)
-        connection.commit()
-
-	def setLastName(self,lname):
-		query = "UPDATE lastName SET user=%s  WHERE name=%s"
-        data = (user, self.name,)
-        cursor.execute(query, data)
-        connection.commit()
-
-	def setEmail(self,mail):
-		query = "UPDATE email SET user=%s  WHERE name=%s"
-        data = (user, self.name,)
-        cursor.execute(query, data)
-        connection.commit()
-
-	def setPassword(self,password):
-		query = "UPDATE password SET user=%s  WHERE name=%s"
-        data = (user, self.name,)
-        cursor.execute(query, data)
-        connection.commit()
-
-	def setAddress(self,add):
-		query = "UPDATE address SET user=%s  WHERE name=%s"
         data = (user, self.name,)
         cursor.execute(query, data)
         connection.commit()
